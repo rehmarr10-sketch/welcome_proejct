a9f7fa0522e6c9ef0e103ba3c353124a47d16efc
rrah0025 <rrah0025@student.monash.edu>
2025-12-21 12:34:31 +0600

    Finalize Django portfolio project

--- welcome_app/views.py
+++ welcome_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+def projects(request):
+    return render(request, "projects.html")
--- welcome_project/settings.py
+++ welcome_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "welcome_app",   
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -54,7 +57,7 @@ ROOT_URLCONF = "welcome_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "welcome_app" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
